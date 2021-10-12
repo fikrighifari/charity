7e29bcf52988097317e82908b18c1348eadafca6
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2021-10-12 16:42:03 +0700

    add component header welcome promo and footer

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,21 +1,16 @@
 <script>
   import CharityList from '../components/CharityList.svelte';
-  import { charities } from '../data/charities.js';
+  import Header from '../components/Header.svelte'
+  import Welcome from '../components/Welcome.svelte'
+  import Promo from '../components/Promo.svelte'
+  import Footer from '../components/Footer.svelte'
+  import { charities } from '../data/charities.js'
+
   let title = 'Charity';
 </script>
 
-<div>
-  <h1>{title}</h1>
-  <CharityList {charities} />
-</div>
-
-<svelte:head>
-  <!-- <link href  ="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" rel="stylesheet"/> -->
-</svelte:head>
-
-<style>
-  h1 {
-    font-size: 2em;
-    color: brown;
-  }
-</style>
+<Header />
+<Welcome />
+<CharityList {charities} /> 
+<Promo />
+<Footer />
