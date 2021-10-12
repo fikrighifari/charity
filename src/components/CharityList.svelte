495bec76d3307bae6b071bd04e81791581ed8109
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2021-10-12 12:09:49 +0700

    add components and pages

---
+++ src/components/CharityList.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let charities;
+</script>
+
+<div>
+  <h1>Charity List</h1>
+  {#if charities !== undefined}
+    <ul>
+      {#each charities as Charity}
+        <li> {Charity} </li>
+      {/each}
+    </ul>
+  {:else}
+    <h5>Kosong</h5>
+  {/if}
+</div>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,24 @@
+<script>
+  import CharityList from '../components/CharityList.svelte';
+
+  let title = 'Charity';
+
+  let charities = ['Charity 1', 'Charity 2', 'Charity 3', 'Charity 4'];
+</script>
+
+<div>
+  <h1>{title}</h1>
+  <CharityList {charities} />
+  <CharityList />
+</div>
+
+<svelte:head>
+  <!-- <link href  ="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" rel="stylesheet"/> -->
+</svelte:head>
+
+<style>
+  h1 {
+    font-size: 2em;
+    color: brown;
+  }
+</style>
