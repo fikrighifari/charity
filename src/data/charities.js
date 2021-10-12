37b181ab33058be52d39cd91d6442b8438bfec96
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2021-10-12 14:27:19 +0700

    adding dummy data

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://images.pexels.com/photos/6647020/pexels-photo-6647020.jpeg?cs=srgb&dl=pexels-rodnae-productions-6647020.jpg&fm=jpg',
+            pledged: 0,
+            target: 100000,
+            date_end: +new Date('13 November 2021'),
+            profile_photo: 'https://images.pexels.com/photos/5704849/pexels-photo-5704849.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704849.jpg&fm=jpg',
+            profile_name: 'Nala',
+            no_pledges: 0
+
+        }
+    ]
+
+}
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,15 +1,12 @@
 <script>
   import CharityList from '../components/CharityList.svelte';
-
+  import { charities } from '../data/charities.js';
   let title = 'Charity';
-
-  let charities = ['Charity 1', 'Charity 2', 'Charity 3', 'Charity 4'];
 </script>
 
 <div>
   <h1>{title}</h1>
   <CharityList {charities} />
-  <CharityList />
 </div>
 
 <svelte:head>
