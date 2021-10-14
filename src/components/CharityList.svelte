42bc784d444e8830bddd97748a7e94d180a9afe2
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2021-10-14 09:33:09 +0700

    daily commit

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -1,5 +1,25 @@
 <script>
+  import Modal from './Modal.svelte';
   export let charities;
+
+  function calculateFunded(pledged, target) {
+    return  Math.round((1 / (target / pledged)) * 100);
+  }
+
+  function formatCurrency(nominal) {
+    return nominal.toLocaleString('id-ID', {
+      style: "currency",
+      currency: "IDR",
+    });
+  }
+
+  function calculateDaysRemaining(date_end) {
+    const delta = date_end - new Date();
+    const oneDay = 24 * 60 * 60 * 1000;
+    return Math.round(Math.abs(delta / oneDay));
+  }
+
+  
 </script>
 
 <!-- popularCauses section -->
@@ -17,156 +37,176 @@
       <!-- .xs-heading-title END -->
     </div>
     <!-- .row end -->
-    <div class="row">
-      <div class="col-lg-4 col-md-6">
-        <!-- modal goes here -->
-        <!-- Modal -->
-        <div
-          class="modal fade"
-          id="exampleModal"
-          tabindex="-1"
-          role="dialog"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">
-                  Splash Drone 3 a Fully Waterproof Drone that floats
-                </h5>
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form>
-                  <div class="form-group">
-                    <label for="exampleInputAmount">Amount donation</label>
-                    <input
-                      required
-                      type="number"
-                      class="form-control"
-                      id="exampleInputAmount"
-                      aria-describedby="amountHelp"
-                      placeholder="Enter amount"
-                    />
+    {#if charities !== undefined}
+      {#each charities as Charity}
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <!-- modal goes here -->
+            <!-- Modal -->
+            <Modal>
+              <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                      Splash Drone 3 a Fully Waterproof Drone that floats
+                    </h5>
+                    <button
+                      type="button"
+                      class="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
                   </div>
-                  <div class="form-group">
-                    <label for="exampleInputName">Your name</label>
-                    <input
-                      required
-                      type="text"
-                      class="form-control"
-                      id="exampleInputName"
-                      aria-describedby="nameHelp"
-                      placeholder="Enter full name"
-                    />
+                  <div class="modal-body">
+                    <form>
+                      <div class="form-group">
+                        <label for="exampleInputAmount">Amount donation</label>
+                        <input
+                          required
+                          type="number"
+                          class="form-control"
+                          id="exampleInputAmount"
+                          aria-describedby="amountHelp"
+                          placeholder="Enter amount"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName">Your name</label>
+                        <input
+                          required
+                          type="text"
+                          class="form-control"
+                          id="exampleInputName"
+                          aria-describedby="nameHelp"
+                          placeholder="Enter full name"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input
+                          required
+                          type="email"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter email"
+                        />
+                      </div>
+                      <div class="form-check">
+                        <input
+                          type="checkbox"
+                          class="form-check-input"
+                          id="exampleCheck1"
+                        />
+                        <label class="form-check-label" for="exampleCheck1"
+                          >I Agree</label
+                        >
+                      </div>
+                    </form>
                   </div>
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input
-                      required
-                      type="email"
-                      class="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      placeholder="Enter email"
-                    />
-                  </div>
-                  <div class="form-check">
-                    <input
-                      type="checkbox"
-                      class="form-check-input"
-                      id="exampleCheck1"
-                    />
-                    <label class="form-check-label" for="exampleCheck1"
-                      >I Agree</label
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-primary"
+                      >Continue</button
                     >
                   </div>
-                </form>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Continue</button>
+                </div>
               </div>
             </div>
+            </Modal>
+            <div class="xs-popular-item xs-box-shadow">
+              <div class="xs-item-header">
+                <img src="{Charity.thumbnail}" alt="" />
+
+                <div class="xs-skill-bar">
+                  <div class="xs-skill-track">
+                    <p>
+                      <span
+                        class="number-percentage-count number-percentage"
+                        data-value="90"
+                        data-animation-duration="3500">
+                        {calculateFunded(Charity.pledged, Charity.target)}
+                        </span
+                      >%
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <!-- .xs-item-header END -->
+              <div class="xs-item-content">
+                <ul class="xs-simple-tag xs-mb-20">
+                  <li><a href="">{Charity.category}</a></li>
+                </ul>
+
+                <a href="#" class="xs-post-title xs-mb-30"
+                  >{Charity.title}</a
+                >
+
+                <ul class="xs-list-with-content">
+                  <li>{formatCurrency(Charity.pledged)}<span>Pledged</span></li>
+                  <li>
+                    <span
+                      class="number-percentage-count number-percentage"
+                      data-value="90"
+                      data-animation-duration="3500">
+                      {calculateFunded(Charity.pledged, Charity.target)}
+                      </span
+                    >% <span>Funded</span>
+                  </li>
+                  <li>
+                    {calculateDaysRemaining(Charity.date_end)}
+                    <span>Days to go</span></li>
+                </ul>
+
+                <span class="xs-separetor" />
+
+                <div class="row xs-margin-0">
+                  <div class="xs-round-avatar">
+                    <img src="{Charity.profile_photo}" alt="" />
+                  </div>
+                  <div class="xs-avatar-title">
+                    <a href="#"><span>By</span>{Charity.profile_name}</a>
+                  </div>
+                </div>
+
+                <span class="xs-separetor" />
+
+                <a
+                  href="#"
+                  data-toggle="modal"
+                  data-target="#exampleModal"
+                  class="btn btn-primary btn-block"
+                >
+                  Donate This Cause
+                </a>
+              </div>
+              <!-- .xs-item-content END -->
+            </div>
+            <!-- .xs-popular-item END -->
           </div>
         </div>
-        <div class="xs-popular-item xs-box-shadow">
-          <div class="xs-item-header">
-            <img src="assets/images/causes/causes_4.png" alt="" />
-
-            <div class="xs-skill-bar">
-              <div class="xs-skill-track">
-                <p>
-                  <span
-                    class="number-percentage-count number-percentage"
-                    data-value="90"
-                    data-animation-duration="3500">0</span
-                  >%
-                </p>
-              </div>
-            </div>
-          </div>
-          <!-- .xs-item-header END -->
-          <div class="xs-item-content">
-            <ul class="xs-simple-tag xs-mb-20">
-              <li><a href="">Food</a></li>
-            </ul>
-
-            <a href="#" class="xs-post-title xs-mb-30"
-              >Splash Drone 3 a Fully Waterproof Drone that floats</a
-            >
-
-            <ul class="xs-list-with-content">
-              <li>$67,000<span>Pledged</span></li>
-              <li>
-                <span
-                  class="number-percentage-count number-percentage"
-                  data-value="90"
-                  data-animation-duration="3500">0</span
-                >% <span>Funded</span>
-              </li>
-              <li>3<span>Days to go</span></li>
-            </ul>
-
-            <span class="xs-separetor" />
-
-            <div class="row xs-margin-0">
-              <div class="xs-round-avatar">
-                <img src="assets/images/avatar/avatar_1.jpg" alt="" />
-              </div>
-              <div class="xs-avatar-title">
-                <a href="#"><span>By</span>Ema Watson</a>
-              </div>
-            </div>
-
-            <span class="xs-separetor" />
-
-            <a
-              href="#"
-              data-toggle="modal"
-              data-target="#exampleModal"
-              class="btn btn-primary btn-block"
-            >
-              Donate This Cause
-            </a>
-          </div>
-          <!-- .xs-item-content END -->
-        </div>
-        <!-- .xs-popular-item END -->
-      </div>
-    </div>
+      {/each}
+    {/if}
     <!-- .row end -->
   </div>
   <!-- .container end -->
 </section>
 <!-- End popularCauses section -->
+
+<style>
+  .xs-list-with-content {
+    font-size: 12px;
+  }
+</style>
 
 <!-- <div>
   <h1>Charity List</h1>
--- src/data/charities.js
+++ src/data/charities.js
@@ -4,10 +4,10 @@ module.exports = {
             id: 1,
             title: 'First Charity Project',
             category: 'Money',
-            thumbnail: 'https://images.pexels.com/photos/6647020/pexels-photo-6647020.jpeg?cs=srgb&dl=pexels-rodnae-productions-6647020.jpg&fm=jpg',
-            pledged: 0,
+            thumbnail: 'https://images.pexels.com/photos/9169658/pexels-photo-9169658.jpeg?cs=srgb&dl=pexels-ron-lach-9169658.jpg&fm=jpg',
+            pledged: 99000,
             target: 100000,
-            date_end: +new Date('13 November 2021'),
+            date_end: +new Date('14 October 2021'),
             profile_photo: 'https://images.pexels.com/photos/5704849/pexels-photo-5704849.jpeg?cs=srgb&dl=pexels-anastasia-shuraeva-5704849.jpg&fm=jpg',
             profile_name: 'Nala',
             no_pledges: 0
