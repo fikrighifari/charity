3a86027bb670fc6846413ddf6ee553b8d52d39b2
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2021-10-15 13:28:28 +0700

    SPA Feature

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -194,14 +194,14 @@
 
                 <span class="xs-separetor" />
 
-                <button
-                  on:click={handleButton}
+                <a
+                  href="/donation/{Charity.id}"
                   data-toggle="modal"
                   data-target="#exampleModal"
                   class="btn btn-primary btn-block"
                 >
                   Donate This Cause
-                </button>
+              </a>
               </div>
               <!-- .xs-item-content END -->
             </div>
